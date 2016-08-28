45543b19d6b14a0997ac0d4f6eeded6eaead0439
poyhsiao <white.nitify@gmail.com>
2016-08-28 16:39:23 +0800

    - Put setting and putils to global variable - Add installDB for installation of db schemas

--- src/init.js
+++ src/init.js
@@ -9,7 +9,7 @@ import express from 'express';
 
 import settings from './settings/global.js';
 import route from './settings/route.js';
-import { logger, sequelize, initialModel } from './settings/utils.js';
+import * as putils from './settings/utils.js';
 
 /**
  * System global setting
@@ -23,13 +23,16 @@ const SETTINGS = settings();
  */
 const app = express();
 
+/** set settings as global variable */
+app.set('SETTINGS', SETTINGS);
+/** set PUTILS as global function */
+app.set('PUTILS', putils);
+
 /** Database connction */
-app.set('SQL', sequelize(app));
-/** initial all model */
-initialModel(app);
+app.set('SQL', putils.sequelize(app));
 
 /** Log setting */
-logger(app, SETTINGS);
+putils.logger(app, SETTINGS);
 /** routing rule setting */
 route(app);
 
--- src/settings/route.js
+++ src/settings/route.js
@@ -12,10 +12,71 @@ import multer from 'multer';
 
 export default app => {
   const upload = multer();
+  const putils = app.get('PUTILS');
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
 
+  /**
+   *
+   * Installtion of the DB schema
+   *
+   * if query / body is "clean" and the value is '1' will clean up all table and set tables
+   *
+   * if query / body is "clean" and the value is not equal '1' will set tables without any data lost
+   *
+   */
+  app.route('/installDB')
+    .post((req, res) => {
+      if ('clean' in req.body) {
+        if (req.body.clean === '1') {
+          putils.initialModel(app, true, () => {
+            res.json({
+              status: 'OK',
+              msg: 'Initial model completed (all data has already cleaned)',
+            });
+          });
+        } else {
+          putils.initialModel(app, false, () => {
+            res.json({
+              status: 'OK',
+              msg: 'Initial model completed (all data is preserved)',
+            });
+          });
+        }
+      } else {
+        res.json({
+          status: 'OK',
+          msg: 'Please ask administrator to handle this',
+        });
+      }
+    })
+    .get((req, res) => {
+      if ('clean' in req.query) {
+        if (req.query.clean === '1') {
+          putils.initialModel(app, true, () => {
+            res.json({
+              status: 'OK',
+              msg: 'Initial model completed (all data has already cleaned)',
+            });
+          });
+        } else {
+          putils.initialModel(app, false, () => {
+            res.json({
+              status: 'OK',
+              msg: 'Initial model completed (all data is preserved)',
+            });
+          });
+        }
+      } else {
+        res.json({
+          status: 'OK',
+          msg: 'Please ask administrator to handle this',
+        });
+      }
+    });
+
+  /** default route rule */
   app.route('/')
     .get((req, res) => {
       const { query } = req;
